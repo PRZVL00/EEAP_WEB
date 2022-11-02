6920368f76033c7bc50a0d97beb11da76c61973d
PRZVL00 <josiahbautista00.com>
2022-11-02 16:08:55 +0800

    try

--- Outputthesis/EEAP/EEAP_app/models.py
+++ Outputthesis/EEAP/EEAP_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here. weh
+# Create your models here.
 
 # class accounts(AbstractUser):
 #     id = models.AutoField(primary_key = True, unique = True)
@@ -15,7 +15,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class student(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
+    id = models.AutoField(primary_key=True, unique=False)
     fname = models.CharField(max_length=20)
     lname = models.CharField(max_length=20)
     birthday = models.DateField()
